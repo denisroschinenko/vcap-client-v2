155653f1fadbf38f6dd1f94cd8916757d2a6a6c0
denisroschinenko <denis.roschinenko@altoros.com>
2013-09-25 17:41:13 +0300

    was added vcapexception

---
+++ src/IronFoundry.VcapClient.V2/Vcap/VcapException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IronFoundry.VcapClient.V2
+{
+    public class VcapException : Exception
+    {
+        public VcapException(string message)
+            : base(message)
+        {
+
+        }
+    }
+}
